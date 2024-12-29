389022ca482e25315b67dcb0f49ee4bc21a8bfbc
swapnilverma0209 <SWAPNIL_VERMA@OUTLOOK.COM>
2024-12-29 14:03:45 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_project',  # Name of your project
+    name='pythonappgitactions',  # Name of your project
     version='0.1.0',  # Initial version
     packages=find_packages(),  # Automatically discover and include all packages in the project
     install_requires=[],
@@ -15,7 +15,7 @@ setup(
     description='A short description of your project',  # Short project description
     long_description=open('README.md').read(),  # Long description, typically from a README file
     long_description_content_type='text/markdown',  # Format of the long description (e.g., markdown)
-    url='https://github.com/yourusername/my_project',  # URL to your project repository
+    url='https://github.com/swapnilverma0209/pythonappgitactions',  # URL to your project repository
     classifiers=[  # Additional metadata that categorizes your project
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
